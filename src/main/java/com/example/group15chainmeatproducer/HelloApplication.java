28e7f43bc68ca07a84ee9df4675bb34a25f9704c
sojib-Tech <shajalal.sojib.11@gmail.com>
2025-08-01 01:06:38 +0600

    chec k

--- src/main/java/com/example/group15chainmeatproducer/HelloApplication.java
+++ src/main/java/com/example/group15chainmeatproducer/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Sojib/1.FF Scan Animal ID Before Slaughter.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Sojib/1..FF Scan Animal ID Before Slaughter.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
