64fb7acc2e090889cf51a7c9c33dd0e79c6415ba
sojib-Tech <shajalal.sojib.11@gmail.com>
2025-08-01 19:38:04 +0600

    check

--- src/main/java/com/example/group15chainmeatproducer/Sojib/User2/ECChooseLogisticsPartner.java
+++ src/main/java/com/example/group15chainmeatproducer/Sojib/User2/ECChooseLogisticsPartner.java
@@ -3,5 +3,25 @@ package com.example.group15chainmeatproducer.Sojib.User2;
 public class ECChooseLogisticsPartner
 {
     @javafx.fxml.FXML
+    private TextField C_NAME;
+    @javafx.fxml.FXML
+    private TextField contact_TERMS;
+    @javafx.fxml.FXML
+    private ComboBox logistic_partner;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void submit_logistic_data(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backfront_page13(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirm_terms(ActionEvent actionEvent) {
+    }
+}
