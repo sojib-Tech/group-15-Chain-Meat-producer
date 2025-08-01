5cedc132bb056f954cc05f7ce01c6d88232948a0
sojib-Tech <shajalal.sojib.11@gmail.com>
2025-08-01 19:21:46 +0600

    check

--- src/main/java/com/example/group15chainmeatproducer/Sojib/User2/ECSubmitCustomsInvoiceDetails.java
+++ src/main/java/com/example/group15chainmeatproducer/Sojib/User2/ECSubmitCustomsInvoiceDetails.java
@@ -3,5 +3,27 @@ package com.example.group15chainmeatproducer.Sojib.User2;
 public class ECSubmitCustomsInvoiceDetails
 {
     @javafx.fxml.FXML
+    private TextField shippingCost;
+    @javafx.fxml.FXML
+    private TextField invoiceNumber;
+    @javafx.fxml.FXML
+    private TextField taxNumber;
+    @javafx.fxml.FXML
+    private TextArea productDetails;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void submitInvoice(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void atttchDocuments(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backfrontpage12(ActionEvent actionEvent) {
+    }
+}
