c2d7bd48f6b855facb60fdf593a8042f72b55d6f
sojib-Tech <shajalal.sojib.11@gmail.com>
2025-08-01 13:27:18 +0600

    check

---
+++ src/main/java/com/example/group15chainmeatproducer/Sojib/ECSelectPendingExportOrder.java
@@ -0,0 +1,25 @@
+package com.example.group15chainmeatproducer.Sojib;
+
+public class ECSelectPendingExportOrder
+{
+    @javafx.fxml.FXML
+    private TextField cooridinator_Name;
+    @javafx.fxml.FXML
+    private TableView table_seelect_pending_EXPORT_ORDER;
+    @javafx.fxml.FXML
+    private TableColumn t_;
+    @javafx.fxml.FXML
+    private DatePicker order_date_9;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void confirm_Selection(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void select_Order(ActionEvent actionEvent) {
+    }
+}
