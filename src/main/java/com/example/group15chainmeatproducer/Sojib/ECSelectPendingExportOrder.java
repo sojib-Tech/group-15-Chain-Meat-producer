019dbf2702d54285c8302b8d5b3e6d9491f4bce9
sojib-Tech <shajalal.sojib.11@gmail.com>
2025-08-01 13:30:32 +0600

    INIT

---
+++ src/main/java/com/example/group15chainmeatproducer/Sojib/ECSelectPendingExportOrder.java
@@ -0,0 +1,7 @@
+package com.example.group15chainmeatproducer.Sojib;
+
+public class ECSelectPendingExportOrder
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
--- src/main/java/com/example/group15chainmeatproducer/Sojib/FFScanAnimalIDBeforeSlaughter.java
+++ src/main/java/com/example/group15chainmeatproducer/Sojib/FFScanAnimalIDBeforeSlaughter.java
@@ -3,5 +3,8 @@ package com.example.group15chainmeatproducer.Sojib;
 public class FFScanAnimalIDBeforeSlaughter
 {
     @javafx.fxml.FXML
+    private Label scsn_animal_IdBefore_slaughter;
+
+    @javafx.fxml.FXML
     public void initialize() {
     }}
